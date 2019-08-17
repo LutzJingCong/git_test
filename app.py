9eb97695e7e8d05092ad00056947e8947cc6c9f1
陆静聪 <18603792842@163.com>
2019-08-17 16:14:28 +0800

    app update

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flask import Flask
+import socket
+
+s = socket.socket()
+s.bind(('127.0.0.1', '8899'))
+s.listen()
+
+while True:
+    print('连接成功')
+    c = s.accept()
+    print(c.recv(1024))
